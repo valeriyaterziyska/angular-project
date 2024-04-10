95ea4cb0d0928292a02364109ba5f209bc61147e
Valeria Terziyska <valeria.terziyska@gmail.com>
2024-04-10 09:17:49 +0300

    Add car service

---
+++ client/src/app/car-service.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { CarData } from './types/car-data';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CarServiceService {
+  private API = 'http://localhost:3030/jsonstore/cars';
+  private headers = { 'Content-Type': 'application/json' };
+  constructor(private http: HttpClient) {}
+
+  getAllCars(): Observable<CarData> {
+    return this.http.get<CarData>(`${this.API}`);
+  }
+
+  getSingleCar(id: string): Observable<CarData> {
+    return this.http.post<CarData>(`${this.API}/${id}`, {
+      "id": id,
+    }, {headers: this.headers});
+  }
+
+  createCar(brand: string, model: string, price: number, imagUrl: string): Observable<CarData> {
+    return this.http.post<CarData>(`${this.API}`, {
+      "brand": brand,
+      "model": model,
+      "price": price,
+      "imagUrl": imagUrl,
+    }, {headers: this.headers});
+  }
+
+}
