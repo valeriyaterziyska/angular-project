f5685b98aabd497742bc7b3c33d4d2e7d5b3937c
Valeria Terziyska <valeria.terziyska@gmail.com>
2024-04-10 21:33:11 +0300

    Add test drive component

--- client/src/app/test-drive/test-drive.component.ts
+++ client/src/app/test-drive/test-drive.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { CarServiceService } from '../car-service.service';
 import { Router } from '@angular/router';
+import { CarData } from '../types/car-data';
 
 @Component({
   selector: 'app-test-drive',
   templateUrl: './test-drive.component.html',
   styleUrls: ['./test-drive.component.css']
 })
-export class TestDriveComponent {
+export class TestDriveComponent implements AfterViewInit {
+  public cars: CarData[] = [];
+  public emptyCatalog: boolean = true;
   constructor(private carService: CarServiceService, private router: Router) {}
+
+
+  ngAfterViewInit(): void {
+    this.carService.getAllCars().forEach((car) => {
+      Object.entries(car).map((c) => {
+        console.log(c[1]);
+        if(c[1].testDrive === true) {
+          this.cars.push(c[1]);
+          this.emptyCatalog = false;
+        }
+      });
+    });
+  }
 
   navigateToDetails(id: string) {
     this.router.navigateByUrl(`/car-item-details/${id}`);
